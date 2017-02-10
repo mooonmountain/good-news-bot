0e0648508aa9aa30f997afc80662fb5dc754f043
mooonmountain <mooonmountain@gmail.com>
2017-02-10 14:57:36 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+  consumer_key: 'TfpK2Ojs68ol5lCW0a5NywUM8',  
+  consumer_secret: 'f7oi4ZdU94ffcakVRuQQpQZwzsb75ibMTAkWYWvoqthZ3Twmd5',
+  access_token: '41402187-6PHNnWlx1wU7LRWmmhM8AW2f2MT0m2RTGZ0T3eaS6',  
+  access_token_secret: 'V5NW3IeHBfDTJu2VbxP6lo4ARM8SKCeSHJgcRbKnXLd0a'
+}
+
